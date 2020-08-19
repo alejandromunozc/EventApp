9670038b43b907239491b872f2ff3c273b78f007
alejandromunozc <alejandromunozc@gmail.com>
2020-08-18 22:26:46 -0500

    move failes .env and .env.example to index

--- api/lib/connect.js
+++ api/lib/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { config } = require('../config');
+const { config } = require('../config/');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
