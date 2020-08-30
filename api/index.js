31a8275cfecfd0a92a9daf3c76a68f80931ed960
alejandromunozc <alejandromunozc@gmail.com>
2020-08-30 00:06:46 -0500

    fix headers allow origin x-access

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-access-token, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-access-token, x-access-token, Content-Type, Accept");
     next();
 });
 
