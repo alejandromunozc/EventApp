547b52ab53ced3ea4e11678b2819dd577615b21c
Israel Castro <israel.castro@gmail.com>
2020-09-11 15:58:42 -0500

    [Init] move files to the root

--- event-app-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./Globals.css";
-import App from "./components/App";
-
-const container = document.getElementById("app");
-
-ReactDOM.render(<App />, container);
